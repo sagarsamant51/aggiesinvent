c8e073e1584be1242fbf2f8201ffc4873130aaf4
sujoysaha777 <sujoy_saha@tamu.edu>
2016-04-17 00:26:42 +0000

    first

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160416152314) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "patients", force: :cascade do |t|
     t.string   "name"
     t.integer  "identification"
